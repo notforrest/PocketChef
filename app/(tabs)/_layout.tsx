1b41023badcf4c40f29c47fbf823ba0f77ffd0fc
Kacey Na <kacey.na01@gmail.com>
2024-05-06 02:44:05 -0500

    Started profile page

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -46,6 +46,19 @@ export default function TabLayout() {
           },
         }}
       />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          headerShown: false,
+          headerTitle: "Profile",
+          title: "Profile",
+          tabBarIcon(props) {
+            return (
+              <Ionicons name="person-circle" size={24} color={props.color} />
+            );
+          },
+        }}
+      />
     </Tabs>
   );
 }
